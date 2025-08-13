fa1264e9710253b73e6d35b6f5ce2cbe694b46ba
pmansy1 <168098891+pmansy1@users.noreply.github.com>
2025-08-13 10:19:38 -0400

    set up spring boot with a temp health controller

---
+++ backend/springboot-package/src/main/java/pmansy/demo/FhirLitePatientExplorerApplication.java
@@ -0,0 +1,13 @@
+package pmansy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirLitePatientExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirLitePatientExplorerApplication.class, args);
+	}
+
+}
